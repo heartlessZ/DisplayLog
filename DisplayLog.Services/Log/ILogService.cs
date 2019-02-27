badd23480c286ff545c0ede0596d914b76713fd7
heartlessZ <boomclub@126.com>
2019-02-27 12:18:29 +0800

    新增业务逻辑层

---
+++ DisplayLog.Services/Log/ILogService.cs
@@ -0,0 +1,23 @@
+﻿/****************************************************************
+* MACHINE: DESKTOP-KFVHQEM
+* NAME: boomclub
+* CREATEDATE: 2019/2/27 10:28:57
+* DESC: <DESCRIPTION>
+* **************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayLog.Services.Log
+{
+    /// <summary>
+    /// Summary description for ILogService
+    /// </summary>
+    public interface ILogService : IDependency
+    {
+        byte[] GetBuffer(string clientName, string fileName);
+
+        List<string> GetFilesName(string clientName);
+    }
+}
---
+++ DisplayLog.Web/Services/FileHelper.cs
@@ -0,0 +1,29 @@
+﻿/****************************************************************
+* MACHINE: DESKTOP-KFVHQEM
+* NAME: boomclub
+* CREATEDATE: 2019/2/27 10:14:56
+* DESC: <DESCRIPTION>
+* **************************************************************/
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DisplayLog.Web.Services
+{
+    /// <summary>
+    /// Summary description for SV
+    /// </summary>
+    public static class FileHelper
+    {
+        public static byte[] GetBuffer(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                return null;
+            }
+            return File.ReadAllBytes(filePath);
+        }
+    }
+}
